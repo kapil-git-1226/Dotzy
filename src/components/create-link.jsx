c4c5acd5419eed7b30e9dd41ec7081b57d0382bf
kapil-git-1226 <gangwark057@gmail.com>
2025-07-19 15:47:04 +0530

    domains fixed

--- src/components/create-link.jsx
+++ src/components/create-link.jsx
@@ -21,16 +21,16 @@ import useFetch from "@/hooks/use-fetch";
 
 const CreateLink = () => {
 
-    const {user} = UrlState();
+    const { user } = UrlState();
     const navigate = useNavigate();
     const ref = useRef();
 
     let [searchParams, setSearchParams] = useSearchParams();
-    const longLink = searchParams.get("createNew"); 
+    const longLink = searchParams.get("createNew");
 
     const [errors, setErrors] = useState({});
     const [formValues, setFormValues] = useState({
-        title:"",
+        title: "",
         longUrl: longLink ? longLink : "",
         customUrl: "",
     });
@@ -44,7 +44,7 @@ const CreateLink = () => {
         customUrl: yup.string(),
     });
 
-    const handleChange = (e) =>{
+    const handleChange = (e) => {
         setFormValues({
             ...formValues,
             [e.target.id]: e.target.value,
@@ -53,68 +53,68 @@ const CreateLink = () => {
 
     const { loading, error, data, fn: fnCreateUrl } = useFetch(createUrl, { ...formValues, user_id: user.id });
 
-    useEffect(()=>{
-        if(error === null && data){
+    useEffect(() => {
+        if (error === null && data) {
             navigate(`/link/${data[0].id}`);
         }
     }, [error, data]);
 
-    
-    
+
+
     const createNewLink = async () => {
-        setErrors([]);
-        try{
-            await schema.validate(formValues, {abortingEarly: false});
+        setErrors({});
+        try {
+            await schema.validate(formValues, { abortEarly: false });
             const canvas = ref.current.canvasRef.current;
             const blob = await new Promise((resolve) => canvas.toBlob(resolve));
             await fnCreateUrl(blob);
-        } catch (e){
+        } catch (e) {
             const newErrors = {};
 
-            e?.inner?.forEach((err)=> {
+            e?.inner?.forEach((err) => {
                 newErrors[err.path] = err.message;
             });
             setErrors(newErrors);
         }
     }
 
-  return (
-      <Dialog
-        defaultOpen={longLink}
-        onOpenChange={(res)=>{
-            if(!res) setSearchParams({});
-        }}
-      >
-          <DialogTrigger>
-            <Button variant="destructive">Create New Link</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-md" >
-              <DialogHeader>
-                  <DialogTitle className="font-bold text-2xl">Create New</DialogTitle>
-              </DialogHeader>
+    return (
+        <Dialog
+            defaultOpen={longLink}
+            onOpenChange={(res) => {
+                if (!res) setSearchParams({});
+            }}
+        >
+            <DialogTrigger>
+                <Button variant="destructive">Create New Link</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md" >
+                <DialogHeader>
+                    <DialogTitle className="font-bold text-2xl">Create New</DialogTitle>
+                </DialogHeader>
 
-                {formValues?.longUrl && <QRCode value={formValues?.longUrl} size={250} ref={ref}/>}
+                {formValues?.longUrl && <QRCode value={formValues?.longUrl} size={250} ref={ref} />}
 
-              <Input id="title" placeholder="Short Link's Title " value={formValues.title} onChange={handleChange}/>
-              {errors.title && <Error message={errors.title}/>}
+                <Input id="title" placeholder="Short Link's Title " value={formValues.title} onChange={handleChange} />
+                {errors.title && <Error message={errors.title} />}
 
-              <Input id="longUrl" placeholder="Enter your Loooong URL " value={formValues.longUrl} onChange={handleChange}/>
-              {errors.longUrl && <Error message={errors.title} />}
+                <Input id="longUrl" placeholder="Enter your Loooong URL " value={formValues.longUrl} onChange={handleChange} />
+                {errors.longUrl && <Error message={errors.longUrl} />}
 
-              <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2">
 
-                <Card className="p-2">http://localhost:5173</Card>/
-                  <Input id="customUrl" placeholder="Custom Link (optional) " value={formValues.customUrl} onChange={handleChange}/>
-              </div>
-              {error && <Error message={errors.message} />}
+                    <Card className="p-2">{window.location.origin}</Card>/
+                    <Input id="customUrl" placeholder="Custom Link (optional) " value={formValues.customUrl} onChange={handleChange} />
+                </div>
+                {errors.customUrl && <Error message={errors.customUrl} />}
 
-              <DialogFooter className="sm:justify-start">
-                <Button disabled={loading} onClick={createNewLink} variant="destructive">
-                    {loading ? <BeatLoader size={10} color="white"/> : "Create"}
-                </Button>
-              </DialogFooter>
-          </DialogContent>
-      </Dialog>
+                <DialogFooter className="sm:justify-start">
+                    <Button disabled={loading} onClick={createNewLink} variant="destructive">
+                        {loading ? <BeatLoader size={10} color="white" /> : "Create"}
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     )
 }
 
